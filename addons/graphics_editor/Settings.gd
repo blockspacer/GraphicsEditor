a08067aed1a0e2684b6c85dc4158d540cb53e010
Flairieve <flairieve@gmail.com>
2019-08-06 00:36:52 -0500

    Added grid settings

--- addons/graphics_editor/Settings.gd
+++ addons/graphics_editor/Settings.gd
@@ -3,32 +3,41 @@ extends Control
 
 onready var editor = get_parent()
 onready var canvas_outline = get_parent().get_node("PaintCanvasContainer/PaintCanvas/CanvasOutline")
-var start_time
-var end_time
+onready var visual_grid_1 = get_parent().get_node("PaintCanvasContainer/PaintCanvas/Grids/VisualGrid")
+onready var visual_grid_2 = get_parent().get_node("PaintCanvasContainer/PaintCanvas/Grids/VisualGrid2")
 
 func _ready():
-	#start_time = OS.get_ticks_msec()
+	#CanvasOutline settings
 	get_node("CanvasOutline/Enabled/CheckButton").pressed = canvas_outline.visible
 	get_node("CanvasOutline/Color/ColorPickerButton").color = canvas_outline.color
 	get_node("CanvasOutline/Width/SpinBox").value = canvas_outline.width
-
-func _process(delta):
-#	if get_parent().paint_canvas_node != null:
-#		canvas_outline = get_parent().paint_canvas_node.get_node("CanvasOutline")
-#		end_time = OS.get_ticks_msec()
-#		print("[Settings] Found Editor node in %s seconds!" % [(end_time - start_time) / float(1000)])
-#		set_process(false)
-	pass
-
-func _on_CheckButton_toggled(button_pressed):
-	canvas_outline.visible = button_pressed
-
-func _on_ColorPickerButton_color_changed(color):
-	canvas_outline.color = color
-
-func _on_SpinBox_value_changed(value):
-	canvas_outline.width = value
+	
+	#Grid settings
+	get_node("Grids/Grid1Color/ColorPickerButton").color = visual_grid_1.color
+	get_node("Grids/Grid1Size/SpinBox").value = visual_grid_1.size
+	get_node("Grids/Grid2Color/ColorPickerButton").color = visual_grid_2.color
+	get_node("Grids/Grid2Size/SpinBox").value = visual_grid_2.size
 
 func _on_Ok_pressed():
 	hide()
 
+func _on_CanvasOutline_Enabled_value_changed(button_pressed):
+	canvas_outline.visible = button_pressed
+
+func _on_CanvasOutline_Color_value_changed(color):
+	canvas_outline.color = color
+
+func _on_CanvasOutline_SpinBox_value_changed(value):
+	canvas_outline.width = value
+
+func _on_Grids_Grid1Color_value_changed(color):
+	visual_grid_1.color = color
+
+func _on_Grids_Grid1Size_value_changed(value):
+	visual_grid_1.size = value
+
+func _on_Grids_Grid2Color_value_changed(color):
+	visual_grid_2.color = color
+
+func _on_Grids_Grid2Size_value_changed(value):
+	visual_grid_2.size = value
